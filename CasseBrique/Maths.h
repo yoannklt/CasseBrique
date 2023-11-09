2f7a0772a434d8448e9ffb1055a9f6192ba3ed06
Yoann Kerlogot <yoannkerlogot9@gmail.com>
2023-11-09 16:23:53 +0100

    getNorm()

--- CasseBrique/Maths.h
+++ CasseBrique/Maths.h
@@ -1,8 +1,12 @@
 #pragma once
+
 #include <SFML/System/Vector2.hpp>
 
 namespace Maths
 {
+	sf::Vector2f vect(float startPointX, float startPointY, float endPointX, float endPointY);
+
+	float getNorm(sf::Vector2f vector);
 	sf::Vector2f normalize(sf::Vector2f vector);
 	float convertRadiansToDegrees(float radianAngle);
 
